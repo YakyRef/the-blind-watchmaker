0c10faa49263b95f8152c355af2f3e5826cbf39a
Yaky Refael <yakyre@payoneer.com>
2019-02-14 16:38:31 +0200

    Adding Footer Buttons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-// import PropTypes from 'prop-types';
+import { ActionCreators } from "redux-undo";
+import PropTypes from "prop-types";
+const { undo,redo } = ActionCreators;
 
 class Footer extends Component {
-    render() {
-        return (
-            <div className="footer">
-                <span role="img" aria-label="Back">⬅️ Back</span>
-                <span role="img" aria-label="Next">Next ➡️</span>               
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="footer">
+        <span role="img" aria-label="Back" onClick={this.props.undo}>
+          ⬅️ Back
+        </span>
+        <span role="img" aria-label="Next" onClick={this.props.redo}>
+          Next ➡️
+        </span>
+      </div>
+    );
+  }
 }
 
-// Footer.propTypes = {
-
-// };
-
+Footer.propTypes = {
+  undo: PropTypes.func,
+  redo: PropTypes.func
+};
 
 const mapStateToProps = state => {
-    return {
-      
-    };
-  };
-  
-  export default connect(
-    mapStateToProps,
-    {  }
-  )(Footer);
-  
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  { undo,redo }
+)(Footer);
